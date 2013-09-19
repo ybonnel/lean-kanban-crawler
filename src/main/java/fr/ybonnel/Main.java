afa3c0c7a5cff4b9a8141158a804af046a05d15d
ybonnel <ybonnel@gmail.com>
2013-09-19 10:01:32 +0200

    Remove mongo

--- src/main/java/fr/ybonnel/Main.java
+++ src/main/java/fr/ybonnel/Main.java
@@ -1,17 +1,7 @@
 package fr.ybonnel;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import de.flapdoodle.embed.mongo.MongodExecutable;
-import de.flapdoodle.embed.mongo.MongodProcess;
-import de.flapdoodle.embed.mongo.MongodStarter;
-import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
-import de.flapdoodle.embed.mongo.config.Net;
-import de.flapdoodle.embed.mongo.distribution.Version;
-import de.flapdoodle.embed.process.runtime.Network;
 import fr.ybonnel.crawler.LanyrdCrawler;
 import fr.ybonnel.modele.Schedule;
-import fr.ybonnel.services.MongoService;
 import fr.ybonnel.simpleweb4j.exception.HttpErrorException;
 import fr.ybonnel.simpleweb4j.handlers.Response;
 import fr.ybonnel.simpleweb4j.handlers.Route;
@@ -19,7 +9,6 @@ import fr.ybonnel.simpleweb4j.handlers.RouteParameters;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.net.UnknownHostException;
 import java.util.List;
 
 import static fr.ybonnel.simpleweb4j.SimpleWeb4j.*;
@@ -35,9 +24,7 @@ public class Main {
      * @param port http port to listen.
      * @param waitStop true to wait the stop.
      */
-    public static void startServer(int port, boolean waitStop, int portMongo) throws IOException {
-        startMongoIfNeeded(waitStop, portMongo);
-        bindMongoClient(portMongo);
+    public static void startServer(int port, boolean waitStop) throws IOException {
 
         // Set the http port.
         setPort(port);
@@ -58,45 +45,6 @@ public class Main {
 
         // Start the server.
         start(waitStop);
-        stopMongoIfNeeded(waitStop);
-    }
-
-
-    private static boolean dev = true;
-
-    public static boolean isDev() {
-        return dev;
-    }
-
-    private static MongodExecutable mongodExe = null;
-    private static MongodProcess mongodProc = null;
-
-    private static void stopMongoIfNeeded(boolean waitStop) {
-        if (isDev() && waitStop) {
-            mongodProc.stop();
-            mongodExe.stop();
-        }
-    }
-
-    private static void startMongoIfNeeded(boolean waitStop, int portMongo) throws IOException {
-        if (isDev() && waitStop) {
-            MongodStarter runtime = MongodStarter.getDefaultInstance();
-            mongodExe = runtime.prepare(new MongodConfigBuilder()
-                    .version(Version.Main.PRODUCTION)
-                    .net(new Net(portMongo, Network.localhostIsIPv6()))
-                    .build());
-            mongodProc = mongodExe.start();
-        }
-    }
-
-    private static void bindMongoClient(int portMongo) throws UnknownHostException {
-        if (isDev()) {
-            MongoService.setMongoClient(new MongoClient("localhost", portMongo), "dev");
-        } else {
-            String uriAsString = System.getProperty("MONGOHQ_URL_BEERS");
-            MongoClientURI uri = new MongoClientURI(uriAsString);
-            MongoService.setMongoClient(new MongoClient(uri), uri.getDatabase());
-        }
     }
 
     /**
@@ -106,14 +54,12 @@ public class Main {
         // Heroku
         String herokuPort = System.getenv("PORT");
         if (herokuPort != null) {
-            dev = false;
             return Integer.parseInt(herokuPort);
         }
 
         // Cloudbees
         String cloudbeesPort = System.getProperty("app.port");
         if (cloudbeesPort != null) {
-            dev = false;
             return Integer.parseInt(cloudbeesPort);
         }
 
@@ -121,16 +67,8 @@ public class Main {
         return 9999;
     }
 
-    /**
-     * @return port to use for mongo.
-     */
-    private static int getPortMongo() {
-        // Default port;
-        return 9998;
-    }
-
     public static void main(String[] args) throws IOException {
         // For main, we want to wait the stop.
-        startServer(getPort(), true, getPortMongo());
+        startServer(getPort(), true);
     }
 }
